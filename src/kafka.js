c6272d0d5a0e998e7b6011b2ace8b0960ce0a225
Will <will@coefficientinc.com>
2014-03-14 08:42:40 -0400

    Add a random function and quotes. Register as commonjs and amd module

--- src/kafka.js
+++ src/kafka.js
@@ -1,10 +1,26 @@
+/* jshint define: true, alert: true */
 (function(document) {
     'use strict';
+
+    var quotes = [
+        'You are now a giant insect.',
+        'You have been charged with a crime, the details of which you are not allowed to know.',
+        'No',
+        'Maybe',
+        'Are you sure?',
+        'Yes'
+    ];
+
+    // Get a random integer between min and max
+    function getRandomInteger(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
 
     function Kafka() {
         this.anchors = document.getElementsByTagName('a');
 
         this.shuffle(this.anchors);
+        this.possibleNews(this.anchors);
     }
 
     // Fisher-Yates shuffle courtesy of http://bost.ocks.org/mike/shuffle/
@@ -25,6 +41,36 @@
 
         return array;
     };
+
+    Kafka.prototype.possibleNews = function(array) {
+
+        for (var i = 0; i < array.length; i++) {
+            var random = Math.random();
+            var anchor = array[i];
+
+            if (random < 0.1) {
+                anchor.addEventListener('click', function(event) {
+                    alert(quotes[getRandomInteger(0, quotes.length)]);
+                    event.preventDefault();
+                });
+            }
+        }
+    };
+
+    // CommonJS module
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = Kafka;
+        }
+        exports.Kafka = Kafka;
+    }
+
+    // Register as a named AMD module
+    if (typeof define === 'function' && define.amd) {
+        define('Kafka', [], function() {
+            return Kafka;
+        });
+    }
 
     return new Kafka();
 })(document);
